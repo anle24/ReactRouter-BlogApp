280c7a76ab3f07e4c89cd79ea4082b724a0c1568
Alan Le <alanle0824@gmail.com>
2018-02-22 20:30:03 -0800

    reduxForm 1.0, pre-API request

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field; // ES6 nested destructuring of field
+    const validInput = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className="validInput">
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-      <span className="text-danger">{field.meta.error}</span>
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
@@ -42,6 +48,7 @@ class PostsNew extends Component {
             component={this.renderField}
           />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link className="btn btn-danger" to="/">Cancel</Link>
         </form>
       </div>
     );
